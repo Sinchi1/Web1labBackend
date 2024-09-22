9f3f75e99aa17db4ef5e8e9ae14a2bff6adda2df
Sinchi <truskovsky2017@yandex.ru>
2024-09-22 20:53:14 +0300

    Кипит работа а я в слезах

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -1,5 +1,33 @@
 package org.example
 
+import com.fastcgi.FCGIInterface
+
 fun main() {
-    println("Hello World!")
+
+    var methodName: String
+    var acTime: Long
+    var fcgiInterface = FCGIInterface()
+
+    fun getValuesOfReq(input: String): LinkedHashMap<String,String> {
+        val args: List<String> = input.split("&");
+        val map: LinkedHashMap<String,String> = LinkedHashMap();
+        for (s in args){
+            val arg: List<String> = input.split("=");
+            map.put(arg[0],arg[1]);
+        }
+        return map;
+    }
+
+    while (fcgiInterface.FCGIaccept() >= 0){
+        methodName = FCGIInterface.request.params.getProperty("REQUEST_METHOD")
+        if (methodName.equals("GET")){
+            var req: String? = FCGIInterface.request.params.getProperty("QUERY_STRING")
+            if (req != null) {
+                if (req.isNotEmpty()){
+                    var values: LinkedHashMap<String, String> = getValuesOfReq(req)
+                }
+            }
+        }
+    }
+
 }
