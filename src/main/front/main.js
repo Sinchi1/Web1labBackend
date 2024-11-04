99ae13135e9accd4ae89e450bdafd99319e66d0d
Sinchi <truskovsky2017@yandex.ru>
2024-11-04 23:00:47 +0300

    fin

--- src/main/front/main.js
+++ src/main/front/main.js
@@ -33,6 +33,24 @@ checkboxX.forEach((checkbox) => {
     });
 });
 
+var animateButton = function(e) {
+
+    e.preventDefault;
+    //reset animation
+    e.target.classList.remove('animate');
+
+    e.target.classList.add('animate');
+    setTimeout(function(){
+        e.target.classList.remove('animate');
+    },700);
+};
+
+var bubblyButtons = document.getElementsByClassName("bubbly-button");
+
+for (var i = 0; i < bubblyButtons.length; i++) {
+    bubblyButtons[i].addEventListener('click', animateButton, false);
+}
+
 document.getElementById('send-btn').addEventListener('click', function(event) {
     event.preventDefault();
     const x = document.querySelector('input[name="x"]:checked');
